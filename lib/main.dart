a573ac77f3451c488b535cce739dcf284c870883
osamamahmoud2 <osama255955@gmail.com>
2024-01-17 17:05:22 +0200

    inirilize App

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DocApp());
+}
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
